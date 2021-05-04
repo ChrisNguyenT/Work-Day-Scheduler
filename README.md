76b39e0d0bf923c301db70b417549462d9972fcb
Thangnt <thangnt336@gmail.com>
2021-05-03 21:40:57 -0400

    added color coding functionality to the time blocks as well as initial readme file description

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work-Day-Scheduler
+
+## Description
+
+This project is a simple calendar application that allows the user to save events for each hour of the day. The calendar is color coded depending on the current time. All of the user inputted text is locally stored.
+
+This application uses moment.js, bootstrap, and jquery.
+
+## Link
+
+https://thangnt336.github.io/Work-Day-Scheduler/
+
+## Usage
+
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -21,4 +21,40 @@ $(function() {
     $('#h15 .description').val(localStorage.getItem('h15'));
     $('#h16 .description').val(localStorage.getItem('h16'));
     $('#h17 .description').val(localStorage.getItem('h17'));
+
+    // Function to add color styling to appropriate time blocks
+    function timeTense() {
+        var now = moment().hour();
+
+        // This function loops over every time block and compares it with the current hour
+        $('.time-block').each(function () {
+            // Converts id to an integer and creates an array of the hours separated by 'h'
+            var timeBlock = parseInt($(this).attr('id').split('h')[1]);
+
+            // Compares time block and current time to add/remove appropriate class for each time block
+            if (timeBlock === now) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+                $(this).removeClass('future');
+            }
+            else if (timeBlock < now) {
+                $(this).addClass('past');
+                $(this).removeClass('present');
+                $(this).removeClass('future');
+            }
+            else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        })
+    }
+    timeTense();
 })
+
+// Clear function to remove all saved text
+$(function() {
+    $('.clearBtn').click(function () {
+        localStorage.clear();
+        location.reload();
+    })});
